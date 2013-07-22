f12cf3e34317a20af19e44d263480a5339ca209b
maddoggin <valozuna361@gmail.com>
2013-07-21 21:41:23 -0500

    revert: Simplify OpenGL TLS access on ARM.  &  mips additions
    Change-Id: I4d03849b8e302d57e7becc7194c67f7996295adc

--- libs/utils/tests/Android.mk
+++ libs/utils/tests/Android.mk
@@ -23,11 +23,23 @@ static_libraries := \
     libgtest \
     libgtest_main
 
+c_includes := \
+    external/zlib \
+    external/icu4c/common \
+    bionic \
+    bionic/libstdc++/include \
+    external/gtest/include \
+    external/stlport/stlport
+
+module_tags := eng tests
+
 $(foreach file,$(test_src_files), \
     $(eval include $(CLEAR_VARS)) \
     $(eval LOCAL_SHARED_LIBRARIES := $(shared_libraries)) \
     $(eval LOCAL_STATIC_LIBRARIES := $(static_libraries)) \
+    $(eval LOCAL_C_INCLUDES := $(c_includes)) \
     $(eval LOCAL_SRC_FILES := $(file)) \
     $(eval LOCAL_MODULE := $(notdir $(file:%.cpp=%))) \
-    $(eval include $(BUILD_NATIVE_TEST)) \
+    $(eval LOCAL_MODULE_TAGS := $(module_tags)) \
+    $(eval include $(BUILD_EXECUTABLE)) \
 )
